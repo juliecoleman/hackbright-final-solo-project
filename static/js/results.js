312389ec77c8841e653ffe009b6c27ebc195a2ed
Julie Coleman <juliecoleman820@gmail.com>
2020-06-11 17:07:52 +0000

    Fixed couple bugs in save favorite crop feature

--- static/js/results.js
+++ static/js/results.js
@@ -5,19 +5,31 @@
 // };
 
 
-$('.add-favorite').on('submit', (evt) => {
-  evt.preventDefault();
-  console.log(evt.target)
-  console.log(evt.target.value)
-  console.dir(evt.target)
+// $('.add-favorite').on('submit', (evt) => {
+//   evt.preventDefault();
+//   console.log(evt.target)
+//   console.log(evt.target.value)
+//   console.dir(evt.target)
  // disableFavoriteButton(evt.target);
 
+//   const formInputs = {
+//     'crop_id': $('#crop_id-field').val(),
+//   };
+//   console.log(formInputs)
+
+//   $.post('/create-favorite', formInputs, (res) => {
+//     alert(res);
+//   });
+// });
+
+function addFavorite (id) {
   const formInputs = {
-    'crop_id': $('#crop_id-field').val(),
+    'crop_id': id,
   };
   console.log(formInputs)
-
+  // e.stopPropagation()
   $.post('/create-favorite', formInputs, (res) => {
     alert(res);
   });
-});
+  // return false; 
+}
